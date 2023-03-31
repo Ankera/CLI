e4b89e61ae70f8a7f078dfb956148e748441880e
子木 <zimu@ywwl.com>
2023-03-31 10:58:01 +0800

    feat: 脚手架可以覆盖

--- CLI-ywc/bin/index.js
+++ CLI-ywc/bin/index.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
-
+import { homedir } from 'node:os'
 import { program } from 'commander'
 import inquirer from 'inquirer'
+import { pathExistsSync } from 'path-exists'
 import { execa } from 'execa'
 import path from 'node:path'
+import fs from 'node:fs'
 import fse from 'fs-extra'
 import { dirname } from 'dirname-filename-esm'
 
@@ -11,13 +13,22 @@ const __dirname = dirname(import.meta)
 
 const pkgPath = path.resolve(__dirname, '../package.json')
 
+const projectPath = path.join(__dirname, './project.json')
+
 const pkg = fse.readJsonSync(pkgPath)
+
+const projecJSON = fse.readJsonSync(projectPath)
+
+const TEMP_HOME = '.ac-cli'
+
+const PROJECT_CACHE_KEY = '.yw_project_cache_key'
 
 program.version(pkg.version || '1.0.0')
 
 program
   .command('api [id]')
   .option('-b, --batch <type>', '批量apiId')
+  .option('-o, --overwrite', '是否覆盖projectID', false)
   .action(async (apiId, opts) => {
     let newId = []
     if (apiId && !opts.batch) {
@@ -31,56 +42,36 @@ program
       newId = [...new Set(newId)]
     }
 
-    const projectId = await inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'name',
-          message: '请选择项目类型',
-          default: 'PRO_XY_LIVE',
-          choices: [
-            {
-              name: '直播<yw-broadcast>',
-              value: 'PRO_XY_LIVE',
-            },
-            {
-              name: '电狼<yw-dianlang>',
-              value: 'PRO_DIANLANG',
-            },
-            {
-              name: '商品中心<yw-goods>',
-              value: 'PROGOODS',
-            },
-            {
-              name: '商家<yw-supply>',
-              value: 'PROSUPPLIER',
-            },
-            {
-              name: '电虎<yw-dianhu>',
-              value: 'PRO_DIANHU',
-            },
-            {
-              name: '营销<yw-marketing>',
-              value: 'PRO_MARKETING',
-            },
-            {
-              name: '结算<yw-finance>',
-              value: 'PRO_SETTLE',
-            },
-          ],
-        },
-      ])
-      .then((answer) => {
+    let ywId = getYwToken()
+
+    if (!ywId || opts.overwrite) {
+      ywId = await inquirer.prompt([projecJSON]).then((answer) => {
         return answer.name
       })
+      const ywPath = createYwPath()
+      fs.writeFileSync(ywPath, ywId)
+    }
 
     console.log('生成apiID', newId)
 
     for (let i = 0; i < newId.length; i++) {
-      await execa('yw', ['api', newId[i], `--projectId=${projectId}`], {
+      await execa('yw', ['api', newId[i], `--projectId=${ywId}`], {
         stdout: 'inherit',
       })
     }
   })
 
 program.parse(process.argv)
+
+function createYwPath() {
+  return path.resolve(homedir(), TEMP_HOME, PROJECT_CACHE_KEY)
+}
+
+function getYwToken() {
+  const ywPath = createYwPath()
+  if (pathExistsSync(ywPath)) {
+    return fse.readFileSync(ywPath).toString()
+  }
+
+  return null
+}
