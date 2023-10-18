6f685c02b23d7723e31595802326c21cb25919c3
ankera <15189120919@163.com>
2023-10-18 23:06:19 +0800

    feat: GIT 配置及其新增chalk

--- CLI-github-gitlab/bin/index.js
+++ CLI-github-gitlab/bin/index.js
@@ -6,7 +6,9 @@ import path from 'node:path'
 import fs from 'node:fs'
 import cp from 'node:child_process'
 import { dirname } from 'dirname-filename-esm'
+import chalk from 'chalk';
 
+const hex = chalk.bold.hex;
 const __dirname = dirname(import.meta)
 
 const pkgPath = path.resolve(__dirname, '../package.json')
@@ -24,7 +26,7 @@ program.command('git').action(async () => {
       defaultValue: 1,
       choices: [
         {
-          name: '阿里云',
+          name: '阿里云-司顺',
           value: 1,
         },
         {
@@ -45,7 +47,7 @@ program.command('git').action(async () => {
 
   if (gitID === 1) {
     cp.execSync(`git config --global user.name  "余亚勇";`)
-    cp.execSync(`git config --global user.email  "15189120919@139.com";`)
+    cp.execSync(`git config --global user.email  "15189120919@63.com";`)
   }
 
   if (gitID === 2) {
@@ -58,17 +60,24 @@ program.command('git').action(async () => {
     cp.execSync(`git config --global user.email  "15189120919@139.com";`)
   }
 
-  console.log('============ 切换成功  ============')
+  console.log(`${chalk.bold.hex('#f2f2f2')('=============')} ${chalk.bold.hex('#52c41a')('切换成功')} ${chalk.bold.hex('#f2f2f2')('=============')}`)
   let res = cp.execSync('git config user.name;git config user.email;')
   res = res.toString().split('\n')
-  console.log(`git user.name: ${res[0]}`)
-  console.log(`git user.email: ${res[1]}`)
+
+  console.log(`git user.name: ${chalk.bold.bgRed(res[0])}`)
+  console.log(`git user.email: ${chalk.bold.bgRed(res[1])}`)
+})
+
+program.command('look').action(async () => {
+  console.log(`${chalk.bold.hex('#f2f2f2')('=============')} ${chalk.bold.hex('#52c41a')('查看本地GIT配置文件')} ${chalk.bold.hex('#f2f2f2')('=============')}`)
+  const res = cp.execSync('git config --list;');
+  console.log(`%c${res.toString()}`, 'color: red;')
 })
 
 // 撤销本地commit
-program.command('uc').action(async () => {
+program.command('reset').action(async () => {
   cp.execSync(`git reset --soft HEAD^`)
-  console.log('============ 本地commit撤销成功  ============')
+  console.log(`${chalk.bold.hex('#f2f2f2')('=============')} ${chalk.bold.hex('#52c41a')('本地commit撤销成功')} ${chalk.bold.hex('#f2f2f2')('=============')}`)
 })
 
 program.parse(process.argv)
